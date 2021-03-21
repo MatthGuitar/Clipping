4d98343d680ef7fc08a0cbcc04ac7cfe49f62679
Matheus Pereira <matheuspereirac@hotmail.com>
2021-03-21 13:06:10 -0300

    Mobile styles

--- components/Aside.js
+++ components/Aside.js
@@ -1,10 +1,17 @@
 import Nav from './Nav'
 import Visits from './Visits'
+import Logo from './Logo'
+import Menubtn from './Menubtn'
+import AsideStyles from '../styles/Sidebar.module.scss'
 
 
 const Aside = () => {
   return (
-    <aside class='sidebar'>
+    <aside class={AsideStyles.sidebar}>
+      <div class={AsideStyles.mobile_header}>
+        <Menubtn />
+        <Logo />
+      </div>
       <Nav />
       <Visits />
     </aside>
--- components/Menubtn.js
+++ components/Menubtn.js
@@ -2,14 +2,17 @@ import Image from 'next/image'
 import MenuStyle from '../styles/Menu.module.scss'
 
 const Menubtn = () => {
+  function slideMenu(event) {
+    console.log(event.target)
+  }
   return (
-    <div class={MenuStyle.menu}>
-      <Image 
-      src='/../public/list.svg'
-      alt='menu hamburger'
-      width={50}
-      height={50}
-      />
+    <div class={MenuStyle.menu} id='mobile_menu' onClick={slideMenu}>
+        <Image 
+        src='/../public/list.svg'
+        alt='menu hamburger'
+        width={50}
+        height={50}
+        />
     </div>
   )
 }
